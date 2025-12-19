7cc1089344da0eb63fa5a57b504d77174238717d
abdocado <abdullahar_@hotmail.com>
2025-12-19 11:13:49 -0500

    initial

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,20 @@
+import requests
+def emotion_detector(text):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    obj = { "raw_document": { "text": text } }
+    response = requests.post(url, json = obj, headers=header)
+    json_response = response.json()
+    my_item = json_response["emotionPredictions"][0]["emotion"]
+    max_val = max(my_item, key=my_item.get)
+    final_repsonse = {"anger":my_item["anger"],
+                      "disgust":my_item["disgust"],
+                      "fear":my_item["fear"],
+                      "joy":my_item["joy"],
+                      "sadness":my_item["sadness"],
+                      "dominant_emotion":max_val
+                    }
+    return final_repsonse
+
+    # return json_response
+    
